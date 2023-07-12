f45cbfa71b5d0e937fcfaa6ed57111311f400b2b
DDesmonts <139395028+DDesmonts@users.noreply.github.com>
2023-07-12 15:48:49 -0600

    Update Proteome_parsing.R

--- R/Proteome_parsing.R
+++ R/Proteome_parsing.R
@@ -1,53 +1,45 @@
-#Parsing and match using the biomaRt package
-#Example using bostaurus 
+#Parsing proteomes using the biomaRt package
+ 
+#To install BiomaRt, enter the following lines: 
+  if (!require("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
 
-require('biomaRt')
+  BiocManager::install("biomaRt")
 
-listEnsembl()
-ensembl <- useEnsembl(biomart = "genes")
-datasets <- listDatasets(ensembl)
-head(datasets)a
-searchDatasets(mart = ensembl, pattern = "btaurus")
-#21 btaurus_gene_ensembl Cow genes (ARS-UCD1.2) ARS-UCD1.2
-ensembl <- useDataset(dataset = "btaurus_gene_ensembl", mart = ensembl)
+#Load biomaRt 
+  require('biomaRt')
+
+#BiomaRt example using btaurus
+
+  listEnsembl()
+  ensembl <- useEnsembl(biomart = "genes")
+  datasets <- listDatasets(ensembl)
+  head(datasets)a
+  searchDatasets(mart = ensembl, pattern = "btaurus")
+  #21 btaurus_gene_ensembl Cow genes (ARS-UCD1.2) ARS-UCD1.2
+  ensembl <- useDataset(dataset = "btaurus_gene_ensembl", mart = ensembl)
 
 #filters = listFilters(ensembl)
 #attributes = listAttributes(ensembl)
   #Searching peptide sequence is too long for biomart and timeout
     #search by chromosomes
 
-chrom = c(1,2)
-annotLookup <- getBM(
-  filters=c("chromosome_name"),
-  values=list(chrom),
-  mart = ensembl,
-  attributes = c(
-    'external_gene_name',
-    'uniprot_gn_id',
-    'chromosome_name',
-    'start_position',
-    'end_position',
-    'peptide'),
-  uniqueRows=TRUE)
+  chrom = c(1,2, "X")
+  annotLookup <- getBM(
+    filters=c("chromosome_name"),
+    values=list(chrom),
+    mart = ensembl,
+    attributes = c(
+      'external_gene_name',
+      'uniprot_gn_id',
+      'chromosome_name',
+      'start_position',
+      'end_position',
+      'peptide'),
+    uniqueRows=TRUE)
 
-head(subset(annotLookup, uniprot_gn_id != ''), 20)[,-4]
-write.csv(annotLookup,file.choose(),row.names = FALSE)
-
-# chrom = c(1,2,"X")
-# annotLookup <- getBM(
-#   filters=c("chromosome_name"),
-#   values=list(chrom),
-#   mart = ensembl,
-# attributes = c(
-#   'ensembl_gene_id',
-#   'ensembl_peptide_id',
-#   'uniprot_gn_id',
-#   'chromosome_name',
-#   'start_position',
-#   'end_position',
-#   'peptide'),
-# uniqueRows=TRUE)
-
+  head(subset(annotLookup, uniprot_gn_id != ''), 20)[,-4]
+  write.csv(annotLookup,file.choose(),row.names = FALSE)
 
 # Combine .csv files
 filepath <- 'C:/Projectfiletest/btaurus_proteome_Uniprot'
@@ -63,26 +55,23 @@ for (i in 2:length(file_list)){
   file_path_full <- paste(filepath,'/',file_list[i],sep = "")
   f<-rbind(f, read.csv(file =file_path_full, header = TRUE))
 }
-#write.csv(f,file.choose(),row.names = FALSE)
+write.csv(f,file.choose(),row.names = FALSE)
 
-##Remove genes that do not possess known protein sequences 
+#Remove genes that do not possess known protein sequences 
 
 fclean <- f[!(f$peptide=="Sequence unavailable"),]
 write.csv(fclean,file.choose(),row.names = FALSE)
 
 ## Add quotation marks back in
-# Read the original CSV file
-data <- read.csv(file.choose(),header=T)
 
+data <- read.csv(file.choose(),header=T)
 # Specify the delimiter used in the CSV file
 delimiter <- ","
-
 # Add quotation marks to each value
 quoted_data <- lapply(data, function(column) {
   quoted_values <- paste0('"', column, '"')
   return(quoted_values)
 })
-
 write.csv(quoted_data, file.choose(), row.names = FALSE, quote = FALSE)
 
 # Write the modified data to a new CSV file
@@ -95,14 +84,16 @@ write.csv(quoted_data, output_file, row.names = FALSE, quote = FALSE)
 
 # To upload the proteome on DigestR you need to:
 
-# add the proteome to DigestR species list and species files in 2017_09_16_rNMR_TravisR
-### Search for: speciesList and speciesFiles in the global environment
-### i.e. speciesList = c('Homo sapiens', 'Plasmodium falciparum (3D7)', 'Pseudomonas aeruginosa 01', 
+# In 2017_09_16_rNMR_Travis_DD_Edits.R
+
+# Search for: speciesList and speciesFiles in the global environment, name the species and add the .csv file
+# i.e. speciesList = c('Homo sapiens', 'Plasmodium falciparum (3D7)', 'Pseudomonas aeruginosa 01', 
 #                 'Pseudomonas aeruginosa 14',), speciesFiles = c('chrHs.csv', 'chr3D7.csv', 'pa01.csv', 'pa14.csv',),
 
-#add loadSpecies in digestomics_MK4.R
+# In digestR_Code_Dimitri_final.R
+# add the csv file to the loadspecies function 
 
-# Modify and loadgene functions based on the following model: 
+# Modify loadgene functions based on the following model: 
 # initTaurus <- function(sFileName = '')
 # {
 #   if (sFileName == '')
@@ -113,7 +104,7 @@ write.csv(quoted_data, output_file, row.names = FALSE, quote = FALSE)
 #   return(taurus)
 # }
 # 
-# #	loadBosTaurusGenes()
+# #	loadTaurusGenes()
 # loadTaurusGenes <- function(sTaurusFilename = "")
 # {
 #   ## Read Human Proteome to Genome Map file
