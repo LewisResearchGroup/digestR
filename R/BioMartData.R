857fd07344c5742a3a45de21c50e0ddb1e0743b5
Sören Wacker <3391614+sorenwacker@users.noreply.github.com>
2023-08-09 12:23:21 -0600

    Update BioMartData.R

--- R/BioMartData.R
+++ R/BioMartData.R
@@ -1,23 +1,3 @@
-.onAttach <- function(libname, pkgname) {
-  if (!requireNamespace("biomaRt", quietly = TRUE)) {
-    if (!requireNamespace("BiocManager", quietly = TRUE)) {
-      install.packages("BiocManager")
-    }
-    BiocManager::install("biomaRt", ask = FALSE)
-    message("`biomaRt` was missing and has been installed.")
-  }
-}
-
-
-.onLoad <- function(libname, pkgname) {
-  if (!requireNamespace("biomaRt", quietly = TRUE)) {
-    if (!requireNamespace("BiocManager", quietly = TRUE)) {
-      install.packages("BiocManager")
-    }
-    BiocManager::install("biomaRt", ask = FALSE)
-    message("`biomaRt` was missing and has been installed.")
-  }
-}
 
 file_path <- system.file("logger.R", package = "digestR")
 print(file_path)
