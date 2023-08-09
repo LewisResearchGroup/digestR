0ec17f8addac245154409daad460939c59af739b
Soren Wacker <swacker@ucalgary.ca>
2023-08-09 17:33:59 -0600

    ..

---
+++ R/utils.R
@@ -0,0 +1,11 @@
+#' @importFrom grDevices X11 bringToTop cm.colors col2rgb contourLines dev.cur dev.list dev.new dev.next dev.off dev.set rgb topo.colors windows
+NULL
+
+#' @importFrom graphics .filled.contour abline axis box contour grconvertX grconvertY image legend lines locator par persp points polygon rect segments text title
+NULL
+
+#' @importFrom stats approx fivenum median na.exclude na.omit sd smooth.spline
+NULL
+
+#' @importFrom utils Rprof available.packages browseURL capture.output choose.dir compareVersion contrib.url download.file flush.console install.packages installed.packages packageDescription read.csv read.table select.list tail tar untar update.packages winMenuAdd winMenuAddItem winMenuNames write.csv write.table
+NULL
