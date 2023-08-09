4e286311289c4505e8b90235fd19f4d9a4548e14
Sören Wacker <3391614+sorenwacker@users.noreply.github.com>
2023-08-09 15:03:14 -0600

    Update aaa.R

--- R/aaa.R
+++ R/aaa.R
@@ -1,9 +1,9 @@
-.onAttach <- function(libname, pkgname) {
-  if (!requireNamespace("biomaRt", quietly = TRUE)) {
-    if (!requireNamespace("BiocManager", quietly = TRUE)) {
-      install.packages("BiocManager")
-    }
-    BiocManager::install("biomaRt", ask = FALSE)
-    message("`biomaRt` was missing and has been installed.")
-  }
-}
+#.onAttach <- function(libname, pkgname) {
+#  if (!requireNamespace("biomaRt", quietly = TRUE)) {
+#    if (!requireNamespace("BiocManager", quietly = TRUE)) {
+#      install.packages("BiocManager")
+#    }
+#    BiocManager::install("biomaRt", ask = FALSE)
+#    message("`biomaRt` was missing and has been installed.")
+#  }
+#}
