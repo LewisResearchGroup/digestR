6a65cd52d4a369a5ba785d86d0531de299e35dab
Soren Wacker <swacker@ucalgary.ca>
2023-08-10 13:25:00 -0600

    tidy up

--- R/utils.R
+++ R/utils.R
@@ -9,3 +9,26 @@ NULL
 
 #' @importFrom utils Rprof available.packages browseURL capture.output choose.dir compareVersion contrib.url download.file flush.console install.packages installed.packages packageDescription read.csv read.table select.list tail tar untar update.packages winMenuAdd winMenuAddItem winMenuNames write.csv write.table
 NULL
+
+
+maybeCreateDirectory <- function(dir) {
+  # Check if the directory exists
+  if (!dir.exists(dir)) {
+    # If the directory does not exist, create it
+    dir.create(dir, recursive = TRUE)  # The recursive = TRUE argument means that it will create any necessary parent directories as well
+  }
+}
+
+
+set_directory <- function(dir) {
+  # Check if the directory exists
+  if (!dir.exists(dir)) {
+    # If the directory does not exist, create it
+    dir.create(dir, recursive = TRUE)  # The recursive = TRUE argument means that it will create any necessary parent directories as well
+  }
+
+  # Set the working directory
+  setwd(dir)
+
+  log_message('Working directory:', getwd())
+}
