cbf28ac9aebc1443f5d07613b93f9a1ae23bc003
Soren Wacker <swacker@ucalgary.ca>
2023-08-09 21:00:33 -0600

    ..

--- R/utils.R
+++ R/utils.R
@@ -9,3 +9,6 @@ NULL
 
 #' @importFrom utils Rprof available.packages browseURL capture.output choose.dir compareVersion contrib.url download.file flush.console install.packages installed.packages packageDescription read.csv read.table select.list tail tar untar update.packages winMenuAdd winMenuAddItem winMenuNames write.csv write.table
 NULL
+
+#' @importFrom windows bringToTop
+NULL
