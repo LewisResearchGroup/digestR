63db76aaf18e655bbab12bfe79d01bcb0f13f86f
DDesmonts <139395028+DDesmonts@users.noreply.github.com>
2023-08-17 10:13:28 -0600

    Update digestR.R

--- R/digestR.R
+++ R/digestR.R
@@ -37,8 +37,8 @@ library(Rcpp)
 library(ggplot2)
 library(ggridges)
 library(dplyr)
-
-
+library(magrittr)
+library(biomaRt)
 
 
 ## Assigns objects to the global environment and creates an undo point
@@ -23033,6 +23033,142 @@ plotCutSite <- function(prot, colour, protCutSites, colorEntry) {
 				       
 # Call the function 
 # display_protease_cut_sites()
+#############################################################################################
+				       
+#' BioMartData R6 class
+#'
+#' An R6 class to fetch, manipulate, and save gene data from the Ensembl BioMart 
+#' database. It allows fetching data for specific chromosomes and datasets, 
+#' and stores all the fetched data internally for further processing.
+#'
+#' @field ensembl The Ensembl BioMart object used for fetching data.
+#' @field dataset The dataset from which data is being fetched.
+#' @field chromosomes The list of chromosomes for which data will be fetched.
+#' @field combined_data A combined data frame of all the fetched data.
+#'
+#' @description 
+#' The `initialize` method sets up the Ensembl BioMart object based on the provided `biomart` 
+#' and `dataset` parameters. It also initializes the `chromosomes` field with the list of 
+#' chromosomes available in the dataset.
+#'
+#' The `get_data` method fetches gene data for specified chromosomes or for all available 
+#' chromosomes if none are specified. It also manages directory creation, error handling, 
+#' data combination, and temporary file handling.
+#'
+#' @examples
+#' # Create an instance of BioMartData
+#' biomart <- BioMartData$new(biomart = "ensembl", dataset = "btaurus_gene_ensembl")
+#' 
+#' # Retrieve and process the data
+#' biomart$get_data(chromosomes = c("1", "2"), filepath = ".")
+#' 
+#' @export
+BioMartData <- R6::R6Class(
+  "BioMartData",
+  public = list(
+    ensembl = NULL,
+    dataset = NULL,
+    chromosomes = NULL,
+    combined_data = NULL,
+
+    #' @description Initialize BioMartData object.
+    #' @param biomart Character string. The biomart to use.
+    #' @param dataset Character string. The dataset to use.
+    initialize = function(biomart, dataset) {
+      self$ensembl <- useEnsembl(biomart = biomart)
+      self$ensembl <- useDataset(dataset = dataset, mart = self$ensembl)
+      self$dataset <- dataset
+      self$chromosomes <- getBM(attributes = 'chromosome_name', mart = self$ensembl)[, 1]
+    },
+
+
+    #' @description Fetch gene data from Ensembl BioMart.
+    #' @param chromosomes Character vector (default NULL). The chromosomes for which data will be fetched.
+    #' @param filepath Character string (default PROTEOMES_PATH). Path where the fetched data will be stored.
+    get_data = function(chromosomes = NULL, filepath = PROTEOMES_PATH) {
+      
+      maybe_create_directory(filepath)
+
+      if (!is.null(chromosomes)) {
+        chromosomes <- intersect(chromosomes, self$chromosomes)
+      } else {
+        chromosomes <- self$chromosomes
+      }
+
+      log_message("Fetching data for chromosomes...")
+      data_list <- list()
+      file_list <- list()  # Create a list to hold the filenames
+
+      for (chrom in chromosomes) {
+        log_message(paste("Fetching data for chromosome", chrom))
+        tryCatch({
+          annotLookup <- getBM(
+            filters = c("chromosome_name"),
+            values = list(chrom),
+            mart = self$ensembl,
+            attributes = c(
+              'external_gene_name',
+              'uniprot_gn_id',
+              'chromosome_name',
+              'start_position',
+              'end_position',
+              'peptide'
+            ),
+            uniqueRows = TRUE
+          )
+
+          annotLookup <- subset(annotLookup, uniprot_gn_id != '')
+          filename <- file.path(filepath, paste0("annotLookup_", self$dataset, "_", chrom, ".csv"))
+          write.csv(annotLookup, filename, row.names = FALSE)
+          data_list[[chrom]] <- annotLookup
+          file_list <- c(file_list, filename)  # Add the filename to the list
+        },
+        error = function(e) {
+          log_message(paste("Failed to fetch data for chromosome", chrom, "due to error:", e))
+        })
+      }
+
+      log_message("Data fetching completed.")
+
+      self$combined_data <- do.call(rbind, data_list)
+      self$combined_data <- self$combined_data[!(self$combined_data$peptide == "Sequence unavailable"), ]
+
+      self$combined_data <- self$combined_data[, c("external_gene_name", "uniprot_gn_id", "chromosome_name", "start_position", "end_position", "peptide")]
+      
+      colnames(self$combined_data) <- c("GeneName", "SwissID", "chrom", "start", "end", "seq")
+
+      combined_filename <- file.path(filepath, paste0(self$dataset, "_combined.csv"))
+      file_list <- c(file_list, combined_filename)
+      write.csv(self$combined_data, combined_filename, row.names = FALSE)
+      
+      self$combined_data <- lapply(self$combined_data, function(column) {
+        quoted_values <- paste0('"', column, '"')
+        return(quoted_values)
+      })
+
+      quoted_filename <- file.path(filepath, paste0(self$dataset, ".csv"))
+      write.csv(self$combined_data, quoted_filename, row.names = FALSE, quote = FALSE)
+      
+      # Remove the temporary files
+      log_message(paste("Removing temporary files:", file_list))
+
+      full_file_paths <- normalizePath(file.path(file_list), mustWork = FALSE)
+      file.remove(full_file_paths)
+      
+      log_message("Downloading proteome completed.")
+    
+    }
+  )
+)
+
+# Create an instance of BioMartData
+# biomart <- BioMartData$new(biomart = "ensembl", dataset = "btaurus_gene_ensembl")
+
+# Retrieve and process the data
+# biomart$get_data()
+
+# To download specific chromosomes, use the following:
+# biomart$get_data(chromosomes = c("1", "2"))
 
 #############################################################################################
 # Importing new proteomes using bioMart
@@ -23108,11 +23244,10 @@ onSearchClick <- function() {
   btn <- tkbutton(tt, text = "Download proteome", command = onDownloadClick)
 
   # Position controls in the window using grid layout with padding
-  # Position controls in the window using grid layout with padding
-tkgrid(lab1, ent1, padx=10, pady=10)
-tkgrid(lab2, ent2, padx=10, pady=10)
-tkgrid(lab3, ent3, padx=10, pady=10)
-tkgrid(lab4, ent4, padx=10, pady=10)
+  tkgrid(lab1, ent1, padx=10, pady=10)
+  tkgrid(lab2, ent2, padx=10, pady=10)
+  tkgrid(lab3, ent3, padx=10, pady=10)
+  tkgrid(lab4, ent4, padx=10, pady=10)
 
 # Create a new frame to hold the buttons
 buttonFrame <- tkframe(tt)
@@ -23125,14 +23260,14 @@ tkgrid(searchBtn, column=0, row=5, padx=10, pady=20, sticky="e")
   tkfocus(tt)
 }
 
-
+##############################################################################################
 generate_proteome <- function() {
   library(tcltk2)
   
   tt <- tktoplevel()
   tkwm.title(tt, "DigestR BioMart Downloader")
   tkwm.geometry(tt, "800x800")
-
+  #DatasetFrame <- ttklabelframe(tt, text='Dataset Results')
   biomartVar <- tclVar("genes")
   datasetVar <- tclVar("btaurus_gene_ensembl")
   chromosomesVar <- tclVar("1, 2")
@@ -23147,11 +23282,8 @@ generate_proteome <- function() {
     }
   }
   
-  speciesLabelFrame <- ttklabelframe(tt, text='Species')
-	
-  lab1 <- tklabel(tt, text = "Biomart")
-  #ent1 <- tkentry(tt, textvariable = biomartVar, width=30)
   # Create a combobox for Biomart options:
+  lab1 <- tklabel(tt, text = "Biomart")
   biomartOptions <- c("genes", "ensembl")
   ent1 <- tk2combobox(tt, values=biomartOptions, textvariable=biomartVar, state="readonly")
   tclvalue(ent1) <- "genes"  
@@ -23159,8 +23291,8 @@ generate_proteome <- function() {
   lab2 <- tklabel(tt, text = "Dataset Results")
   listbox <- tklistbox(tt, height=20, width=100, font='Consolas 10', selectmode='extended', active='dotbox', exportselection=FALSE, bg='white',
 			xscrollcommand=function(...) tkset(xscr, ...), yscrollcommand=function(...) tkset(yscr, ...))
-  xscr <- ttkscrollbar(speciesLabelFrame, orient='horizontal', command=function(...) tkxview(listbox, ...))
-  yscr <- ttkscrollbar(speciesLabelFrame, orient='vertical',	command=function(...) tkyview(listbox, ...))
+  #xscr <- ttkscrollbar(speciesLabelFrame, orient='horizontal', command=function(...) tkxview(listbox, ...))
+  #yscr <- ttkscrollbar(speciesLabelFrame, orient='vertical',	command=function(...) tkyview(listbox, ...))
   tkbind(listbox, "<Double-1>", set_dataset)
   
   lab3 <- tklabel(tt, text = "Chromosomes")
@@ -23215,6 +23347,96 @@ generate_proteome <- function() {
 }
 generate_proteome()
 
+###############################################################################################
+generate_proteome <- function() {
+  library(tcltk2)
 
- 
+  tt <- tktoplevel()
+  tkwm.title(tt, "DigestR BioMart Downloader")
+  tkwm.geometry(tt, "800x800")
+  
+  DatasetFrame <- ttklabelframe(tt, text='Dataset Results')
+  
+  biomartVar <- tclVar("genes")
+  datasetVar <- tclVar("btaurus_gene_ensembl")
+  chromosomesVar <- tclVar("1, 2")
+  searchPatternVar <- tclVar("taurus")
+  
+  set_dataset <- function(...) {
+    selected_index <- tclvalue(tkcurselection(listbox))
+    if (length(selected_index) > 0) {
+      selected_entry <- tkget(listbox, selected_index)
+      dataset <- unlist(strsplit(selected_entry, "\\|"))[1]
+      tclvalue(datasetVar) <- dataset
+    }
+  }
+  
+  lab1 <- tklabel(tt, text = "Biomart")
+  biomartOptions <- c("genes", "ensembl")
+  ent1 <- tk2combobox(tt, values = biomartOptions, textvariable = biomartVar, state = "readonly")
+  tclvalue(ent1) <- "genes"
+  
+  lab2 <- tklabel(tt, text = "Dataset Results")
+  listbox <- tklistbox(DatasetFrame, height = 20, width = 100, font = 'Consolas 10', selectmode = 'extended', active = 'dotbox', exportselection = FALSE, bg = 'white')
+  yscr <- ttkscrollbar(DatasetFrame, orient = 'vertical', command = function(...) tkyview(listbox, ...))
+  tkgrid(listbox, yscr)
+  tkbind(listbox, "<Double-1>", set_dataset)
+  
+  lab3 <- tklabel(tt, text = "Chromosomes")
+  ent3 <- tkentry(tt, textvariable = chromosomesVar, width = 30)
+  
+  lab4 <- tklabel(tt, text = "Search Datasets")
+  ent4 <- tkentry(tt, textvariable = searchPatternVar, width = 30)
+  
+  populate_listbox <- function(search_results) {
+    tkdelete(listbox, 0, "end")
+    for (i in 1:nrow(search_results)) {
+      entry <- with(search_results[i, ], {
+        sprintf("%-30s | %-60s | %-20s", dataset, description, version)
+      })
+      tkinsert(listbox, "end", entry)
+    }
+  }
+  
+  onSearchClick <- function() {
+    search_pattern <- tclvalue(searchPatternVar)
+    if (search_pattern != "") {
+      ensembl <- useEnsembl(biomart = "genes")
+      search_results <- searchDatasets(mart = ensembl, pattern = search_pattern)
+      populate_listbox(search_results)
+    }
+    }
+  }
+  
+  searchBtn <- tkbutton(tt, text = "Search Datasets", command = onSearchClick)
+  
+  onDownloadClick <- function() {
+    mart <- tclvalue(biomartVar)
+    dataset <- tclvalue(datasetVar)
+    chromosomes <- tclvalue(chromosomesVar)
+    
+    if (chromosomes == "") {
+      chromosome_list <- NULL
+    } else {
+      chromosome_list <- strsplit(chromosomes, ", ?")[[1]]
+    }
+    
+   biomart <- BioMartData$new(biomart = mart, dataset = dataset)
+   biomart$get_data(chromosomes = chromosome_list)
+  }
+  
+  btn <- tkbutton(tt, text = "Download proteome", command = onDownloadClick)
+  
+  tkgrid(lab1, ent1, padx = 10, pady = 10)
+  tkgrid(lab2, DatasetFrame, padx = 10, pady = 10)
+  tkgrid(lab3, ent3, padx = 10, pady = 10)
+  tkgrid(lab4, ent4, padx = 10, pady = 10)
+  tkgrid(searchBtn, btn, padx = 10, pady = 20)
+  
+  # Run the Tkinter event loop
+  tkpack(tt)
+}
+
+generate_proteome()
+
 
