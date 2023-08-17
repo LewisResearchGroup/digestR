348be19085935e8fda46f9abb479e03772c41ee7
Soren Wacker <swacker@ucalgary.ca>
2023-08-17 13:27:37 -0600

    namespace issue

--- R/digestR.R
+++ R/digestR.R
@@ -17318,7 +17318,7 @@ process_mascot <- function()
 #'                     a file dialog will prompt the user to choose a save location.
 #' @return None (invisible return).
 #'
-#' @importFrom tcltk mySave saveAs fileFolder wc nzchar
+#' @importFrom tcltk mySave saveAs wc nzchar
 #' @export
 sa <- function(saveFileName = '')
 {
