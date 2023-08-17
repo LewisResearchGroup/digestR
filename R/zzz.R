f2fd06c6d30f20424d6ec1a9005db7f96ae9795f
Soren Wacker <swacker@ucalgary.ca>
2023-08-17 11:45:36 -0600

    added zzz.R

---
+++ R/zzz.R
@@ -0,0 +1,10 @@
+# Aliases
+gp <- generate_proteome
+cs <- display_protease_cut_sites
+csd <- cut_sites_distribution
+pd <- peptides_distribution
+up <- unique_peptides
+rd <- prepare_mascot
+pm <- process_mascot
+fo <- file_open
+
