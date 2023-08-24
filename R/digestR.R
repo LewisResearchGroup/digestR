15a089d0475eb628a977600b3083f64973733587
DDesmonts <139395028+DDesmonts@users.noreply.github.com>
2023-08-24 13:48:02 -0600

    Update digestR.R

--- R/digestR.R
+++ R/digestR.R
@@ -1019,7 +1019,7 @@ gui <- function(top=NULL){
     winMenuAddItem("Manipulate csv", 'Remove Duplicates                   prepare_mascot/rd()', "prepare_mascot_files/rd()")
     winMenuAddItem("Manipulate csv", 'Unique peptides                     unique_peptides/up()', "unique_peptides/up()")
     winMenuAddItem("Manipulate csv", 'Process Mascot Files                process_mascot/pm()', "process_mascot/pm()")
-    winMenuAddItem("Manipulate csv", 'Generate New Proteome               generate_proteome/np()', "generate_proteome/gp()")
+    winMenuAddItem("Manipulate csv", 'Generate New Proteome               generate_proteome/gp()', "generate_proteome/gp()")
     winMenuAddItem("Manipulate csv", 'Plot cut site distribution          cut_sites_distribution/csd()', "cut_sites_distribution/csd()")
     winMenuAddItem("Manipulate csv", 'Plot peptide distribution           peptides_distribution/pd()', "peptides_distribution/pd()")
     winMenuAddItem("Manipulate csv", 'Import Maven Files                  im()', "im()")
