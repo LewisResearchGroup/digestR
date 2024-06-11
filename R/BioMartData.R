8e3c8c7e8b76564c8622bca8f60767ce2144aa56
Soren Wacker <s.wacker@tudelft.nl>
2024-06-11 10:11:52 +0200

    fixed function

--- R/BioMartData.R
+++ R/BioMartData.R
@@ -1,6 +1,18 @@
 library(magrittr)
 library(biomaRt)
+library(R6)
 
+# Placeholder for maybe_create_directory
+maybe_create_directory <- function(filepath) {
+  if (!dir.exists(filepath)) {
+    dir.create(filepath, recursive = TRUE)
+  }
+}
+
+# Placeholder for log_message
+log_message <- function(message) {
+  cat(paste0("[INFO] ", message, "\n"))
+}
 
 #' BioMartData R6 class
 #'
@@ -30,7 +42,7 @@ library(biomaRt)
 #' biomart$get_data(chromosomes = c("1", "2"), filepath = ".")
 #' 
 #' @export
-BioMartData <- R6::R6Class(
+BioMartData <- R6Class(
   "BioMartData",
   public = list(
     ensembl = NULL,
@@ -48,30 +60,28 @@ BioMartData <- R6::R6Class(
       self$chromosomes <- getBM(attributes = 'chromosome_name', mart = self$ensembl)[, 1]
     },
 
-
     #' @description Fetch gene data from Ensembl BioMart.
     #' @param chromosomes Character vector (default NULL). The chromosomes for which data will be fetched.
     #' @param filepath Character string (default PROTEOMES_PATH). Path where the fetched data will be stored.
     get_data = function(chromosomes = NULL, filepath = PROTEOMES_PATH) {
-      
       maybe_create_directory(filepath)
-
+      
       if (!is.null(chromosomes)) {
         chromosomes <- intersect(chromosomes, self$chromosomes)
       } else {
         chromosomes <- self$chromosomes
       }
-
+      
       log_message("Fetching data for chromosomes...")
       data_list <- list()
       file_list <- list()  # Create a list to hold the filenames
-
+      
       for (chrom in chromosomes) {
         log_message(paste("Fetching data for chromosome", chrom))
         tryCatch({
           annotLookup <- getBM(
             filters = c("chromosome_name"),
-            values = list(chromosome),
+            values = list(chrom),
             mart = self$ensembl,
             attributes = c(
               'external_gene_name',
@@ -83,7 +93,7 @@ BioMartData <- R6::R6Class(
             ),
             uniqueRows = TRUE
           )
-
+          
           annotLookup <- subset(annotLookup, uniprot_gn_id != '')
           filename <- file.path(filepath, paste0("annotLookup_", self$dataset, "_", chrom, ".csv"))
           write.csv(annotLookup, filename, row.names = FALSE)
@@ -91,19 +101,19 @@ BioMartData <- R6::R6Class(
           file_list <- c(file_list, filename)  # Add the filename to the list
         },
         error = function(e) {
-          log_message(paste("Failed to fetch data for chromosome", chromosome, "due to error:", e))
+          log_message(paste("Failed to fetch data for chromosome", chrom, "due to error:", e))
         })
       }
-
+      
       log_message("Data fetching completed.")
-
+      
       self$combined_data <- do.call(rbind, data_list)
       self$combined_data <- self$combined_data[!(self$combined_data$peptide == "Sequence unavailable"), ]
-
+      
       self$combined_data <- self$combined_data[, c("external_gene_name", "uniprot_gn_id", "chromosome_name", "start_position", "end_position", "peptide")]
       
       colnames(self$combined_data) <- c("GeneName", "SwissID", "chromosome", "start", "end", "seq")
-
+      
       combined_filename <- file.path(filepath, paste0(self$dataset, "_combined.csv"))
       file_list <- c(file_list, combined_filename)
       write.csv(self$combined_data, combined_filename, row.names = FALSE)
@@ -112,18 +122,17 @@ BioMartData <- R6::R6Class(
         quoted_values <- paste0('"', column, '"')
         return(quoted_values)
       })
-
+      
       quoted_filename <- file.path(filepath, paste0(self$dataset, ".csv"))
       write.csv(self$combined_data, quoted_filename, row.names = FALSE, quote = FALSE)
       
       # Remove the temporary files
       log_message(paste("Removing temporary files:", file_list))
-
+      
       full_file_paths <- normalizePath(file.path(file_list), mustWork = FALSE)
       file.remove(full_file_paths)
       
       log_message("Downloading proteome completed.")
-    
     }
   )
 )
