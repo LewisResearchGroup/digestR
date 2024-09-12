5c97dfc04fca31dc869b542cac002eea9edc835f
DDesmonts <139395028+DDesmonts@users.noreply.github.com>
2024-09-11 19:42:53 -0600

    Update digestR.R

--- R/digestR.R
+++ R/digestR.R
@@ -17909,101 +17909,101 @@ look_up_species_files <- function(){
 #'
 #' @import tcltk
 #' @export
-process_mascot <- function()
-{
+# process_mascot <- function()
+# {
 
-  species <- look_up_species_files()
+#   species <- look_up_species_files()
 
-  ##creates main window
-  tclCheck()
-  dlg <- myToplevel('pm')
-  if (is.null(dlg))
-    return(invisible())
+#   ##creates main window
+#   tclCheck()
+#   dlg <- myToplevel('pm')
+#   if (is.null(dlg))
+#     return(invisible())
   
-  tkwm.title(dlg, 'Process Mascot Files')
-  tkfocus(dlg)
-  tkwm.deiconify(dlg)
+#   tkwm.title(dlg, 'Process Mascot Files')
+#   tkfocus(dlg)
+#   tkwm.deiconify(dlg)
   
-  speciesLabelFrame <- ttklabelframe(dlg, text='Species')
+#   speciesLabelFrame <- ttklabelframe(dlg, text='Species')
   
-  speciesList <- tclVar()
-  tclObj(speciesList) <- species
+#   speciesList <- tclVar()
+#   tclObj(speciesList) <- species
   
-  tmp <- tclvalue(tkfont.actual(dlg))
+#   tmp <- tclvalue(tkfont.actual(dlg))
   
-  tmp2 <- strsplit(tmp, ' ', fixed=TRUE)
-  fam <- tmp2[[1]][2]
-  siz <- tmp2[[1]][4]
-  wgt <- tmp2[[1]][6]
-  slt <- tmp2[[1]][8]
-  slt <- 'italic' ## change to italics
+#   tmp2 <- strsplit(tmp, ' ', fixed=TRUE)
+#   fam <- tmp2[[1]][2]
+#   siz <- tmp2[[1]][4]
+#   wgt <- tmp2[[1]][6]
+#   slt <- tmp2[[1]][8]
+#   slt <- 'italic' ## change to italics
   
-  myFont <- tkfont.create(family = fam, size = siz, weight = wgt, slant = slt)
+#   myFont <- tkfont.create(family = fam, size = siz, weight = wgt, slant = slt)
   
-  speciesBox <- tklistbox(speciesLabelFrame, height=10, width=34, listvariable=speciesList,
-                          selectmode='extended', active='dotbox',	exportselection=FALSE, bg='white', font = myFont,
-                          xscrollcommand=function(...) tkset(xscr, ...), yscrollcommand=function(...) tkset(yscr, ...))
+#   speciesBox <- tklistbox(speciesLabelFrame, height=10, width=34, listvariable=speciesList,
+#                           selectmode='extended', active='dotbox',	exportselection=FALSE, bg='white', font = myFont,
+#                           xscrollcommand=function(...) tkset(xscr, ...), yscrollcommand=function(...) tkset(yscr, ...))
   
-  xscr <- ttkscrollbar(speciesLabelFrame, orient='horizontal', command=function(...) tkxview(speciesBox, ...))
-  yscr <- ttkscrollbar(speciesLabelFrame, orient='vertical',	command=function(...) tkyview(speciesBox, ...))
+#   xscr <- ttkscrollbar(speciesLabelFrame, orient='horizontal', command=function(...) tkxview(speciesBox, ...))
+#   yscr <- ttkscrollbar(speciesLabelFrame, orient='vertical',	command=function(...) tkyview(speciesBox, ...))
   
-  if (length(species) > 2)
-  {
-    for (i in seq(0, length(species) - 1, 2))
-      tkitemconfigure(speciesBox, i, background='#ececff')
-  }
+#   if (length(species) > 2)
+#   {
+#     for (i in seq(0, length(species) - 1, 2))
+#       tkitemconfigure(speciesBox, i, background='#ececff')
+#   }
   
-  tkselection.set(speciesBox, 0)	
-  #################
-  ##create single vs. multiple file conversion radiobuttons
-  fileConvFrame <- ttklabelframe(dlg, text='Convert File(s):')
-  singleFileConvVal <- tclVar(TRUE)
+#   tkselection.set(speciesBox, 0)	
+#   #################
+#   ##create single vs. multiple file conversion radiobuttons
+#   fileConvFrame <- ttklabelframe(dlg, text='Convert File(s):')
+#   singleFileConvVal <- tclVar(TRUE)
   
-  singleFileOnButton <- ttkradiobutton(fileConvFrame, variable=singleFileConvVal, 
-                                       value=TRUE, text='Single File')
+#   singleFileOnButton <- ttkradiobutton(fileConvFrame, variable=singleFileConvVal, 
+#                                        value=TRUE, text='Single File')
   
-  singleFileOffButton <- ttkradiobutton(fileConvFrame, variable=singleFileConvVal, 
-                                        value=FALSE, text='All Files in folder and sub folders')
-  #################
-  buttonFrame <- ttkframe(dlg)
+#   singleFileOffButton <- ttkradiobutton(fileConvFrame, variable=singleFileConvVal, 
+#                                         value=FALSE, text='All Files in folder and sub folders')
+#   #################
+#   buttonFrame <- ttkframe(dlg)
   
-  onApply <- function()
-  {
-    idx <- 1 + as.integer(tkcurselection(speciesBox))
-    #selectedSpeciesName <- basename(species[idx]) # Added
-    globalSettings$processSpeciesID <<- idx
-    globalSettings$processSingleFile <<- (as.logical(tclObj(singleFileConvVal)) == TRUE)
-    tkdestroy(dlg)
-    #loadProteome(species[idx], selectedSpeciesName)  # Added Pass the selected file and name
-    batchConvert()
-  }
-  apply <- ttkbutton(buttonFrame, text='Apply', width=10, command=onApply)
+#   onApply <- function()
+#   {
+#     idx <- 1 + as.integer(tkcurselection(speciesBox))
+#     #selectedSpeciesName <- basename(species[idx]) # Added
+#     globalSettings$processSpeciesID <<- idx
+#     globalSettings$processSingleFile <<- (as.logical(tclObj(singleFileConvVal)) == TRUE)
+#     tkdestroy(dlg)
+#     #loadProteome(species[idx], selectedSpeciesName)  # Added Pass the selected file and name
+#     batchConvert()
+#   }
+#   apply <- ttkbutton(buttonFrame, text='Apply', width=10, command=onApply)
   
-  ##add widgets to speciesFrame
-  tkgrid(speciesLabelFrame, column=1, row=1, sticky='nswe', pady=6, padx=6)
-  tkgrid(speciesBox, column=1, row=1, sticky='nswe')
-  tkgrid(yscr, column=2, row=1, sticky='ns')
-  tkgrid(xscr, column=1, row=2, sticky='we')
+#   ##add widgets to speciesFrame
+#   tkgrid(speciesLabelFrame, column=1, row=1, sticky='nswe', pady=6, padx=6)
+#   tkgrid(speciesBox, column=1, row=1, sticky='nswe')
+#   tkgrid(yscr, column=2, row=1, sticky='ns')
+#   tkgrid(xscr, column=1, row=2, sticky='we')
   
-  ##make fileFrame stretch when window is resized
-  tkgrid.columnconfigure(dlg, 1, weight=1)
-  tkgrid.rowconfigure(dlg, 1, weight=10)
-  tkgrid.columnconfigure(speciesLabelFrame, 1, weight=1)
-  tkgrid.rowconfigure(speciesLabelFrame, 1, weight=1)
+#   ##make fileFrame stretch when window is resized
+#   tkgrid.columnconfigure(dlg, 1, weight=1)
+#   tkgrid.rowconfigure(dlg, 1, weight=10)
+#   tkgrid.columnconfigure(speciesLabelFrame, 1, weight=1)
+#   tkgrid.rowconfigure(speciesLabelFrame, 1, weight=1)
   
-  tkgrid(fileConvFrame, column=1, row=2, sticky='we', pady=6, padx=6)
-  tkgrid(singleFileOnButton, column=1, row=1, sticky='nswe', pady=6, padx=6)
-  tkgrid(singleFileOffButton, column=2, row=1, sticky='nswe', pady=6, padx=6)
+#   tkgrid(fileConvFrame, column=1, row=2, sticky='we', pady=6, padx=6)
+#   tkgrid(singleFileOnButton, column=1, row=1, sticky='nswe', pady=6, padx=6)
+#   tkgrid(singleFileOffButton, column=2, row=1, sticky='nswe', pady=6, padx=6)
   
-  tkgrid(buttonFrame, column=1, row=3, sticky='nswe', pady=6, padx=6)
-  tkgrid(apply, column=1, row=1, sticky='w')
-  tkgrid.columnconfigure(buttonFrame, 1, weight=1)
-  tkgrid.rowconfigure(buttonFrame, 1, weight=1)
+#   tkgrid(buttonFrame, column=1, row=3, sticky='nswe', pady=6, padx=6)
+#   tkgrid(apply, column=1, row=1, sticky='w')
+#   tkgrid.columnconfigure(buttonFrame, 1, weight=1)
+#   tkgrid.rowconfigure(buttonFrame, 1, weight=1)
   
-  tkgrid(ttksizegrip(dlg), column=2, row=3, sticky='se')
+#   tkgrid(ttksizegrip(dlg), column=2, row=3, sticky='se')
   
-  invisible()
-}
+#   invisible()
+# }
 ######
 process_mascot <- function()
 {
@@ -18023,8 +18023,8 @@ process_mascot <- function()
   tkwm.deiconify(dlg)
   tkraise(dlg)
 
-  tkupdate()
-  
+  tcl("update")
+
   speciesLabelFrame <- ttklabelframe(dlg, text='Species')
   
   speciesList <- tclVar()
